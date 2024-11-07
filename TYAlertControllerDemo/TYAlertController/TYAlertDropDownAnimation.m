ea9e9687aaa3b7d30c7db8f9bc8b8b7157171d90
ShiQiangMu <952472674@qq.com>
2024-11-07 15:39:45 +0800

    Update TYAlertDropDownAnimation.m
    腾讯广告插屏冲突

--- TYAlertControllerDemo/TYAlertController/TYAlertDropDownAnimation.m
+++ TYAlertControllerDemo/TYAlertController/TYAlertDropDownAnimation.m
@@ -20,44 +20,17 @@
 
 - (void)presentAnimateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
 {
-    TYAlertController *alertController = (TYAlertController *)[transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIViewController *vc = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
     
-    alertController.backgroundView.alpha = 0.0;
-    
-    switch (alertController.preferredStyle) {
-        case TYAlertControllerStyleAlert:
-            alertController.alertView.transform = CGAffineTransformMakeTranslation(0, -CGRectGetMaxY(alertController.alertView.frame));
-            break;
-        case TYAlertControllerStyleActionSheet:
-            NSLog(@"don't support ActionSheet style!");
-            break;
-        default:
-            break;
-    }
-    
-    UIView *containerView = [transitionContext containerView];
-    [containerView addSubview:alertController.view];
-
-    [UIView animateWithDuration:0.5 delay:0.0 usingSpringWithDamping:0.65 initialSpringVelocity:0.5 options:0 animations:^{
+    if ([vc isKindOfClass:[TYAlertController class]]) {
         
-        alertController.backgroundView.alpha = 1.0;
-        alertController.alertView.transform = CGAffineTransformIdentity;
+        TYAlertController *alertController = (TYAlertController *)vc;
         
-    } completion:^(BOOL finished) {
-        [transitionContext completeTransition:YES];
-    }];
-}
-
-- (void)dismissAnimateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
-{
-    TYAlertController *alertController = (TYAlertController *)[transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
-    
-    [UIView animateWithDuration:0.25 animations:^{
         alertController.backgroundView.alpha = 0.0;
+        
         switch (alertController.preferredStyle) {
             case TYAlertControllerStyleAlert:
-                alertController.alertView.alpha = 0.0;
-                alertController.alertView.transform = CGAffineTransformMakeScale(0.9, 0.9);
+                alertController.alertView.transform = CGAffineTransformMakeTranslation(0, -CGRectGetMaxY(alertController.alertView.frame));
                 break;
             case TYAlertControllerStyleActionSheet:
                 NSLog(@"don't support ActionSheet style!");
@@ -65,9 +38,58 @@
             default:
                 break;
         }
-    } completion:^(BOOL finished) {
+        
+        UIView *containerView = [transitionContext containerView];
+        [containerView addSubview:alertController.view];
+
+        [UIView animateWithDuration:0.5 delay:0.0 usingSpringWithDamping:0.65 initialSpringVelocity:0.5 options:0 animations:^{
+            
+            alertController.backgroundView.alpha = 1.0;
+            alertController.alertView.transform = CGAffineTransformIdentity;
+            
+        } completion:^(BOOL finished) {
+            [transitionContext completeTransition:YES];
+        }];
+        
+    } else {
+        
         [transitionContext completeTransition:YES];
-    }];
+        
+    }
+
+}
+
+- (void)dismissAnimateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
+{
+    UIViewController *vc = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    
+    if ([vc isKindOfClass:[TYAlertController class]]) {
+        
+        TYAlertController *alertController = (TYAlertController *)vc;
+        
+        [UIView animateWithDuration:0.25 animations:^{
+            alertController.backgroundView.alpha = 0.0;
+            switch (alertController.preferredStyle) {
+                case TYAlertControllerStyleAlert:
+                    alertController.alertView.alpha = 0.0;
+                    alertController.alertView.transform = CGAffineTransformMakeScale(0.9, 0.9);
+                    break;
+                case TYAlertControllerStyleActionSheet:
+                    NSLog(@"don't support ActionSheet style!");
+                    break;
+                default:
+                    break;
+            }
+        } completion:^(BOOL finished) {
+            [transitionContext completeTransition:YES];
+        }];
+        
+    } else {
+        
+        [transitionContext completeTransition:YES];
+        
+    }
+    
 }
 
 @end
